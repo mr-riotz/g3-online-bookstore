ce7bb2ce82f43e7ee0a61d113339ef37ff5e1de2
mr-riotz <142850046+mr-riotz@users.noreply.github.com>
2025-04-11 19:52:28 -0400

    Set up persistent H2 database and seeded initial books

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (title, author, price) VALUES ('The Pragmatic Programmer', 'Andy Hunt', 42.50);
+INSERT INTO book (title, author, price) VALUES ('Clean Code', 'Robert C. Martin', 38.95);
+INSERT INTO book (title, author, price) VALUES ('Effective Java', 'Joshua Bloch', 45.00);
